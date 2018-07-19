5ba74578012f0ee643a204385694b17d71e2fdad
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-19 15:58:11 +0000

    automatically backed up by learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,6 +13,8 @@ class StudentsController < ApplicationController
 
   def create
     @student = Student.new(first_name: params[:first_name], last_name: params[:last_name])
+    @student.save
+    
   end
 
 end
